d6757e008233045a07d910d38a274a13f6938923
Ivo <cervantes_vive@yahoo.com>
2012-01-29 19:06:54 -0800

    dont add rbenv if it doesnt exist

--- plugins/available/rbenv.plugin.bash
+++ plugins/available/rbenv.plugin.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Load rbebv, if you are using it
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+if [ -f ~/.rbenv/completions/rbenv.bash ] && [ -d $HOME/.rbenv/bin ]; then
+	# Load rbebv, if you are using it
+	export PATH="$HOME/.rbenv/bin:$PATH"
+	eval "$(rbenv init -)"
 
-# Load the auto-completion script if rbenv was loaded.
-source ~/.rbenv/completions/rbenv.bash
+	# Load the auto-completion script if rbenv was loaded.
+	source ~/.rbenv/completions/rbenv.bash
+fi
